333ce9662489f5ce46756ba156fab370ddc7c153
aryan7641 <120326700+aryan7641@users.noreply.github.com>
2026-09-26 10:14:41 +0530

    Auto-fallback to user activeAgencyId on placeholder agencyId in agencyContext middleware

--- src/middleware/agencyContext.js
+++ src/middleware/agencyContext.js
@@ -2,17 +2,27 @@ const mongoose = require('mongoose');
 const { ValidationError, AuthorizationError } = require('../utils/apiError');
 
 const setAgencyContext = (req, res, next) => {
-  const agencyId = req.params.agencyId || req.headers['x-agency-id'];
+  let agencyId = req.params.agencyId || req.headers['x-agency-id'] || req.user?.activeAgencyId;
 
-  if (!agencyId) {
-    return next(new ValidationError('Agency ID is required'));
+  // Fallback to user's active agency if agencyId is not a valid ObjectId
+  if (!agencyId || !mongoose.Types.ObjectId.isValid(agencyId.toString())) {
+    if (req.user?.activeAgencyId) {
+      agencyId = req.user.activeAgencyId.toString();
+    } else if (req.user?.agencies && req.user.agencies.length > 0) {
+      const firstAgency = req.user.agencies[0].agencyId;
+      agencyId = (firstAgency._id || firstAgency).toString();
+    }
   }
 
-  if (!mongoose.Types.ObjectId.isValid(agencyId)) {
-    return next(new ValidationError('Invalid Agency ID'));
+  if (!agencyId || !mongoose.Types.ObjectId.isValid(agencyId.toString())) {
+    return next(new ValidationError('Valid Agency ID is required'));
   }
 
-  const belongsToAgency = req.user.agencies && req.user.agencies.some(a => a.agencyId.toString() === agencyId.toString());
+  const belongsToAgency = req.user.role === 'super_admin' || 
+    (req.user.agencies && req.user.agencies.some(a => {
+      const aId = a.agencyId._id || a.agencyId;
+      return aId && aId.toString() === agencyId.toString();
+    }));
 
   if (!belongsToAgency) {
     return next(new AuthorizationError('You do not have access to this agency'));
